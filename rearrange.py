20643840b240446c536ec4708b40e6b59b38e185
TonyCioara <tonyangelo9707@gmail.com>
2017-10-25 15:18:10 -0700

    Rearrange.py complete

---
+++ rearrange.py
@@ -0,0 +1,21 @@
+
+import sys
+import random
+current_input = sys.argv
+
+
+def rearrange(my_list):
+    # Delete first word;
+    my_list.pop(0)
+
+    # Rearrange list into a new list in a random order;
+    new_list = []
+    index = 0
+    while index < len(my_list):
+        rand_num = random.randint(0, len(my_list) - 1)
+        new_list.append(my_list[rand_num])
+        my_list.pop(rand_num)
+    print(new_list)
+
+
+rearrange(current_input)
