94e2d96fa69bf335a1a461622afcc3097acc0f05
TonyCioara <tonyangelo9707@gmail.com>
2017-10-31 12:12:46 -0700

    Added histogram.py. Takes very long time for lists

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,11 +1,12 @@
 import sys
 import random
+import time
 
 
 def dictionary_words():
     # Get words from file
-    with open('/usr/share/dict/words') as f:
-        raw_data = f.readlines()
+    with open('/usr/share/dict/words') as words_file:
+        raw_data = words_file.readlines()
         raw_data_list = list(raw_data)
         words_array = []
         for index in raw_data_list:
@@ -13,18 +14,24 @@ def dictionary_words():
     return words_array
 
 
+# function purpose, params, return values
 def create_sentence(words_array, word_number):
     # Create random sentence
-    sentence = ""
+    sentence = []
     for index in range(0, word_number):
         rand_num = random.randint(0, len(words_array) - 1)
-        sentence += words_array[rand_num] + " "
-        words_array.pop(rand_num)
-    return sentence
+        sentence.append(words_array[rand_num])
+        # del words_array[rand_num]
+    return " ".join(sentence)
 
 
 if __name__ == "__main__":
+    initial_time = time.time()
     current_input = sys.argv
     words_array = dictionary_words()
     sentence = create_sentence(words_array, int(current_input[1]))
+    final_time = time.time()
+    run_time = final_time - initial_time
     print(sentence)
+    print(len(words_array))
+    print("Ran in", run_time)
---
+++ histogram.py
@@ -0,0 +1,72 @@
+import re
+import sys
+
+
+def create_data():
+    """Takes no parameters.
+    Returns a list of all words in the file"""
+    with open('dracula.txt') as words_file:
+        raw_data = words_file.read()
+        raw_data = re.sub('[^0-9a-zA-Z]+', ' ', raw_data)
+        raw_data = raw_data.replace('\n', '')
+        raw_data = raw_data.lower()
+        data_list = raw_data.split(" ")
+        return data_list
+
+
+def histogram_dict(data_list):
+    """Takes in list of words.
+    Returns dictionary of words and frequency"""
+    final_data_dict = {}
+    get = final_data_dict.get
+    for word in data_list:
+        final_data_dict[word] = get(word, 0) + 1
+    return final_data_dict
+
+
+def unique_words(histogram):
+    """Takes in dict histogram.
+    Returns number of unique words"""
+    return len(histogram)
+
+
+def dict_frequency(word, histogram):
+    """Takes in a word and a histogram as dict.
+    Returns the word count for that word"""
+    if word in histogram:
+        return histogram[word]
+    else:
+        return 0
+
+
+def list_frequency(word, histogram):
+    """Takes in a word and a histogram as list.
+    Returns the word count for that word"""
+    for index in histogram:
+        if index[0] == word:
+            return index[1]
+    return 0
+
+
+def histogram_list(data_list):
+    """Takes in list of words.
+    Returns list of lists, containing words and frequency"""
+    final_data_list = [[w, data_list.count(w)] for w in data_list]
+    return(final_data_list)
+
+
+if __name__ == "__main__":
+    current_input = sys.argv[1]
+    input_word = sys.argv[2]
+    if current_input == "dict":
+        data_list = create_data()
+        histogram_dict = histogram_dict(data_list)
+        print(histogram_dict)
+        print("Unique words:", unique_words(histogram_dict))
+        print(input_word, ":", dict_frequency(input_word, histogram_dict))
+    elif current_input == "list":
+        data_list = create_data()
+        histogram_list = histogram_list(data_list)
+        print(histogram_list)
+        print("Unique words:", unique_words(histogram_list))
+        print(input_word, ":", list_frequency(input_word, histogram_list))
