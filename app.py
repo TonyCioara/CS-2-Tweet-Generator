1fffca1c9c0bbeba849eae0cc82ee072494fe4c6
TonyCioara <tonyangelo9707@gmail.com>
2017-11-07 11:14:22 -0800

    Server working and printing out sentence

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from stochastic import Stochastic
+app = Flask(__name__)
+
+
+def main():
+    word_num = 30
+    data_list = Stochastic.create_data()
+    histogram = Stochastic.create_histogram(data_list)
+    sentence = Stochastic.create_sentence(word_num, histogram)
+    return sentence
+
+
+@app.route('/')
+def add_sentence():
+    sentence = main()
+    return sentence
+
+
+if __name__ == '__main__':
+    app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+    app.run(debug=True)
