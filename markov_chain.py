f13d759cd0cf0cd9b6d969b53c67cbb55f9ff750
TonyCioara <tonyangelo9707@gmail.com>
2017-11-14 15:06:16 -0800

    Added start and stop tokens

--- markov_chain.py
+++ markov_chain.py
@@ -37,12 +37,17 @@ class MarkovChain(object):
         with open(self.text_file) as words_file:
             raw_data = words_file.read()
             raw_data = raw_data.replace('\n', ' ')
+            raw_data = raw_data.replace('. ', ' STOP START ')
+            raw_data = raw_data.replace('? ', ' STOP START ')
+            raw_data = raw_data.replace('! ', ' STOP START ')
             raw_data = re.sub('[^a-zA-Z]+', ' ', raw_data)
             data_list = raw_data.split(" ")
             return data_list
 
     def create_markov_chain(self, data_list):
         markov_chain = {}
+        markov_chain['START'] = Dictogram()
+        markov_chain['START'].add_count(data_list[0])
         for index in range(0, len(data_list) - 1):
             word = data_list[index]
             if word not in markov_chain:
@@ -55,11 +60,14 @@ class MarkovChain(object):
         Returns sentence."""
 
         sentence = []
-        last_word = None
+        last_word = 'START'
 
         for index in range(0, word_num):
             word = self.stochastic(last_word)
             last_word = word
+            if last_word == 'STOP':
+                sentence.append(".")
+                return " ".join(sentence)
             sentence.append(word)
         sentence.append(".")
         return " ".join(sentence)
@@ -68,7 +76,7 @@ class MarkovChain(object):
     def stochastic(self, last_word):
         """If last word is passed in get word based on the last word.
         If last word is none, get random word"""
-        if last_word is None:
+        """if last_word is None:
             index = random.randint(1, self.total_word_count)
             for word in self.markov_chain:
                 word_dict = self.markov_chain[word]
@@ -77,14 +85,14 @@ class MarkovChain(object):
                     index -= frequency
                 else:
                     return word
-        else:
-            index = random.randint(1, self.markov_chain[last_word].tokens)
-            for word in self.markov_chain[last_word]:
-                frequency = self.markov_chain[last_word].frequency(word)
-                if index > frequency:
-                    index -= frequency
-                else:
-                    return word
+        else:"""
+        index = random.randint(1, self.markov_chain[last_word].tokens)
+        for word in self.markov_chain[last_word]:
+            frequency = self.markov_chain[last_word].frequency(word)
+            if index > frequency:
+                index -= frequency
+            else:
+                return word
 
 
 if __name__ == "__main__":
